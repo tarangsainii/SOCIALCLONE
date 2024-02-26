eb2f3d8f7e74bd3d47b5977f4dd627e08d9c3337
sainishipra99 <shiprasaini2012@gmail.com>
2024-02-27 00:08:28 +0530

    Restructuring AuthPage Folder

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
   <Routes>
     <Route path='/' element= {<HomePage />} />
-    <Route path='/auth' element= {<AuthPage />} />
+    <Route path='/auth' element= {<AuthPage/>} />
     </Routes>
   </>
   ); 
