08cfff531e4b5e86bbca206f297cd3e042fb0bbf
TARANG <apple@Tarangs-MacBook-Pro.local>
2024-03-03 14:12:27 +0530

    Added Authpage file

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,11 +1,23 @@
-import {Container,Flex } from '@chakra-ui/react';
+import {Container,Flex,VStack,Box,Image} from '@chakra-ui/react';
+
 //import React from 'react'
 
 const AuthPage = () => {
   return (
    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
     <Container maxW={"container.mid"} padding={0}>
-         this is some content
+        {/* Left hand-side */}
+        <Box display={{base:"none",md:"block"}}>
+        <Image src="/auth.png" h={650} alt='Phone img' />
+        </Box>
+        {/* Right hand-side */}
+        <VStack spacing={4} align={"stretch"}>
+     <AuthForm />
+<Box textAlign={"center"}>Get the app. </Box>
+<Flex gap={"5"} justifyContent={"center"}></Flex>
+    <Image src='/playstore.png' h={"10"} alt='Playstore logo' />
+    <Image src='/microsoft.png' h={"10"} alt='Microsoft logo' />
+        </VStack>
     </Container>
    </Flex>
   );
