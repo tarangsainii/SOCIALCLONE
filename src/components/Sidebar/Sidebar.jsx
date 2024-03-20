495d52f73c91ce2653f9e39d5c092a96095d6d1e
TARANG <apple@Tarangs-MacBook-Pro.local>
2024-03-20 14:01:40 +0530

    Added Authpage file

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,9 @@
-import { Box,Flex,Link, Tooltip } from '@chakra-ui/react';
+import { Avatar, Box,Flex,Link, Tooltip } from '@chakra-ui/react';
 import {Link as RouterLink } from 'react-router-dom';
 import {CreatePostLogo, InstagramLogo, InstagramMobileLogo, NotificationsLogo, SearchLogo} from "../../assets/constants";
 
 import {AiFillHome} from "react-icons/ai";
+import {BiLogOut } from "react-icons/bi";
 const Sidebar = () => {
   const sidebarItems = [
     {
@@ -71,6 +72,8 @@ const Sidebar = () => {
            borderRadius={6}
            p={2}
            w={{base:10,md: "full"}}
+           justifyContent={{base:"center", md:"flex-start"}}
+
            >
           {item.icon}
           <Box display={{base:"none", md:"block"}}>
@@ -82,6 +85,33 @@ const Sidebar = () => {
           ))}
 
         </Flex>
+        <Tooltip 
+            hasArrow
+            label={"Logout"}
+            placement='right'
+            ml={1}
+            openDelay={500}
+            display={{base: "block", md:"none"}}
+            >
+          <Link
+           display={"flex"}
+            to={"/auth" }
+            as={RouterLink}
+           alignItems={"center"}
+           gap={4}
+           _hover={{ bg:"whiteAlpha.400" }}
+           borderRadius={6}
+           p={2}
+           w={{base:10,md: "full"}}
+           mt={'auto'}
+           justifyContent={{base:"center", md:"flex-start"}}
+           
+           >
+          <BiLogOut size={25} />
+          <Box display={{base:"none", md:"block"}}>Logout</Box>
+
+          </Link>
+            </Tooltip>
     </Flex>
     
   </Box>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import { Box, Container, Flex } from '@chakra-ui/react';
+import FeedPosts from "../../components/FeedPosts/Feedposts";
+//import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
+  <Container  maxW={"container.1g"} >
+  <Flex gap={20}>
+    <Box flex={2} py={10} border={"1px solid blue"}>
+      < FeedPosts />
+    </Box>
+    <Box flex={3} mr={20}
+    display={{base:"none", lg:"block"}}
+    maxW={"300px"}
+    border={"1px solid red"}
+    >
+      Suggested Users
+    </Box>
+  </Flex>
+
+  </Container>
+  );
 };
 
 export default HomePage;
