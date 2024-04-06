443b1dfaec0164c36cdcafef8804054b7c86c0e0
sainishipra99 <shiprasaini2012@gmail.com>
2024-04-06 16:57:17 +0530

    added import

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {useSignOut } from 'react-firebase-hooks/auth'
 import { auth } from '../firebase/firebase';
 import useShowToast from './useShowToast';
+import useAuthStore from '../store/authStore';
 
 const useLogout = () => {
   const [signOut, isLoggingOut, error ] = useSignOut(auth);
